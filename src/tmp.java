648a5086f3887173afc60bb4fa38bb70605f373c
Leonard Wahl <lw@lwmb.local>
2022-03-09 10:21:33 +0100

    minor tests

--- src/tmp.java
+++ src/tmp.java
@@ -1,5 +1,11 @@
+import java.lang.reflect.Array;
+
 public class tmp {
-    public static void main(String string){
-        System.out.println(string);
+    public static void main(String[] string){
+        System.out.println("main doing nothing");
+    }
+
+    public static void junk(int str[]){
+        System.out.println(str[0]);
     }
 }
