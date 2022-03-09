446ce3dec68446573e4f67506a202ac1f731e8fb
Leonard Wahl <lw@lwmb.local>
2022-03-09 13:17:19 +0100

    working on input.java

--- src/Input.java
+++ src/Input.java
@@ -1,19 +1,32 @@
+import java.util.Scanner;
+
 public class Input {
     
     public static void main(String[] args){
 
+        System.out.print("Enter the x dimension of your Array: ");
+        Scanner scnr = new Scanner(System.in);
+        int sizex = scnr.nextInt();
+
+        System.out.print("Enter the y dimension of your Array: ");
+        int sizey = scnr.nextInt();
+        scnr.nextLine();// to consume the \n that nextInt leaves over, so that the following nextLine won't be skipped
+
+        if(sizex*sizey > 16){
+            System.out.print("Are you sure that you want to define an Array with " + sizex*sizey + " cells? (y/n): ");
+            Character choice = scnr.nextLine().charAt(0);
+            if(choice != 'y'){
+                return;
+            }
+        }
+
         int arr[][];
-        arr = new int [2][2];
+        arr = new int [sizex][sizey];
         arr[0][0] = 87653;
         arr[1][0] = 234;
         arr[0][1] = 9;
         arr[1][1] = 5436;
 
-        //System.out.println(arr[0][0]);
-
-        //int str[][];
-        //str = new int [1][1];
-        //str[0][0] = 17;
         Output_Engine.print(arr);
     }
 }
--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,5 @@
+echo "Removing Classes"
 rm Output_Engine.class
 
+echo "Compiling Classes"
 javac Output_Engine.java
