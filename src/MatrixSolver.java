5a701849a4dd30826f9d1583153a3338dc04b28b
Leonard Wahl <lw@lwmb.local>
2022-03-13 12:40:02 +0100

    Update MatrixSolver.java

--- src/MatrixSolver.java
+++ src/MatrixSolver.java
@@ -9,7 +9,25 @@ public class MatrixSolver {
         System.out.println(ANSI_RED + "[WARNING]: " + ANSI_RESET + "don't use main, it doesn't do anything, use another method");
         //ONLY USE MAIN METHOD FOR TESTING!!!
         int arr [][][];
-        arr = new int [1][3][4];
+        arr = new int [1][3][3];
+
+        //example equation system array:
+        //String varnames []
+        //varnames = new String [4]
+        //varnames[0] = N
+        //varnames[1] = x
+        //varnames[2] = y
+        //varnames[3] = z
+        //arr[0][0][0] = 9;
+        //arr[0][1][0] = 7;
+        //arr[0][2][0] = 3;
+        //arr[0][0][1] = 1;
+        //arr[0][1][1] = 8;
+        //arr[0][2][1] = 6;
+        //arr[0][0][2] = 4;
+        //arr[0][1][2] = 2;
+        //arr[0][2][2] = 11;
+
         Determinant(arr);
     }
 
@@ -19,13 +37,13 @@ public class MatrixSolver {
         // [x][x][x]
         // [x][x][x]
         // [x][x][x]
-        // [y]
+        // parallel 1-dimensional Array that stores the variable name (var_names[x] = arr[x][][])
 
         System.out.println(ANSI_YELLOW + "[INFO]: " + ANSI_GREEN + "MatrixSolver" + ANSI_RESET + " initalized");
 
         int Determinant[][];
         Determinant = new int [1][3];
-        Determinant[0][0] = arr[0][0][3];
+        Determinant[0][0] = arr[0][0][3]; 
         Determinant[0][1] = arr[0][0][0]*arr[0][1][1]*arr[0][2][2] + arr[0][1][0]*arr[0][2][1]*arr[0][0][2] + arr[0][2][0]*arr[0][0][1]*arr[0][1][2] - arr[0][0][2]*arr[0][1][1]*arr[0][2][0] - arr[0][1][2]*arr[0][2][1]*arr[0][0][0] - arr[0][2][2]*arr[0][0][1]*arr[0][1][0];
 
         Output_Engine.print(Determinant);
