5f99baeab0e67023be39be3909d15b27357565c7
Lprd245 <79790302+Lprd245@users.noreply.github.com>
2022-03-14 16:35:27 +0100

    Update MatrixSolver.java

--- src/MatrixSolver.java
+++ src/MatrixSolver.java
@@ -8,8 +8,8 @@ public class MatrixSolver {
     public static void main(String[] args){
         System.out.println(ANSI_RED + "[WARNING]: " + ANSI_RESET + "don't use main, it doesn't do anything, use another method");
         //ONLY USE MAIN METHOD FOR TESTING!!!
-        int arr [][][];
-        arr = new int [1][3][3];
+        int arr [][];
+        arr = new int [6][3];
 
         //example equation system array:
         String varnames [];
@@ -18,29 +18,29 @@ public class MatrixSolver {
         varnames[1] = "x";
         varnames[2] = "y";
         varnames[3] = "z";
-        arr[0][0][0] = 9;
-        arr[0][1][0] = 7;
-        arr[0][2][0] = 3;
-        arr[0][0][1] = 1;
-        arr[0][1][1] = 8;
-        arr[0][2][1] = 6;
-        arr[0][0][2] = 4;
-        arr[0][1][2] = 2;
-        arr[0][2][2] = 11;
-        arr[1][0][0] = 15;
-        arr[1][0][1] = 6;
-        arr[1][0][2] = 9;
-        arr[2][1][0] = 3;
-        arr[2][1][1] = 2;
-        arr[2][1][2] = 4;
-        arr[2][2][0] = 7;
-        arr[2][2][1] = 27;
-        arr[2][2][2] = 18;
+        arr[0][0] = 9;
+        arr[1][0] = 7;
+        arr[2][0] = 3;
+        arr[0][1] = 1;
+        arr[1][1] = 8;
+        arr[2][1] = 6;
+        arr[0][2] = 4;
+        arr[1][2] = 2;
+        arr[2][2] = 11;
+        arr[3][0] = 15;
+        arr[3][1] = 6;
+        arr[3][2] = 9;
+        arr[4][0] = 3;
+        arr[4][1] = 2;
+        arr[4][2] = 4;
+        arr[5][0] = 7;
+        arr[5][1] = 27;
+        arr[5][2] = 18;
 
         Determinant(arr, varnames);
     }
 
-    public static void Determinant(int arr[][][], String varnames[]){
+    public static void Determinant(int arr[][], String varnames[]){
 
         // 3-dimensional Array of strings with y being the variable to be found for each "layer", form arr[3][4][x]
         // [x][x][x]
@@ -49,11 +49,6 @@ public class MatrixSolver {
         // parallel 1-dimensional Array that stores the variable name (var_names[x] = arr[x][][])
 
         System.out.println(ANSI_YELLOW + "[INFO]: " + ANSI_GREEN + "MatrixSolver" + ANSI_RESET + " initalized");
-
-        int Determinant[][];
-        Determinant = new int [1][3];
-        Determinant[0][0] = arr[0][0][3]; 
-        Determinant[0][1] = arr[0][0][0]*arr[0][1][1]*arr[0][2][2] + arr[0][1][0]*arr[0][2][1]*arr[0][0][2] + arr[0][2][0]*arr[0][0][1]*arr[0][1][2] - arr[0][0][2]*arr[0][1][1]*arr[0][2][0] - arr[0][1][2]*arr[0][2][1]*arr[0][0][0] - arr[0][2][2]*arr[0][0][1]*arr[0][1][0];
 
         Output_Engine.print(Determinant);
 
