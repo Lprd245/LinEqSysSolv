270ab19501dddcc09b4da404b9ba6cc876046714
Laurin Seeholzer <Laurin_Seeholzer@outlook.com>
2022-03-17 14:57:57 +0100

    Update Human_Input_Engine.java

--- src/Human_Input_Engine.java
+++ src/Human_Input_Engine.java
@@ -10,6 +10,7 @@ public class Human_Input_Engine {
 
     public static void main(String[] args) {
         System.out.println(ANSI_RED + "[WARNING]: " + ANSI_RESET + "don't use main, it doesn't do anything, use another method");
+        String x = simplify("1 X-3 (4+1)=3");
     }
 
     public static String reader() {
@@ -36,9 +37,29 @@ public class Human_Input_Engine {
         equation = equation.replaceAll("\\s+", ""); //remove spaces
         equation = equation.toLowerCase(); //lowercase
 
-        //!used for further process
-        //String left_term = equation.split("=")[0]; 
-        //String right_term = equation.split("=")[1];
+        
+        String left_term = equation.split("=")[0]; 
+        String right_term = equation.split("=")[1];
+
+        String new_left_term;
+        String new_right_term;
+
+        //solving brackets
+        for (int i=0; i<left_term.length(); i++) {
+             Boolean closed = true;
+            if (left_term.charAt(i) == '(' && closed == true) {
+                closed = false;
+                String sub_equation = "";
+                for (int j=i+1; j<left_term.length(); j++) {
+                    if (left_term.charAt(j) == ')') {
+                        closed = true;
+                        break;
+                    } else {
+                        sub_equation = sub_equation + left_term.charAt(j);
+                    }
+                }
+            }
+        }   
 
         return equation;
     }
